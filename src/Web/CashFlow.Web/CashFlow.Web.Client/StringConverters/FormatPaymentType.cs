88f03269a514c6732301f95a3c5f10eb94cd47fd
Wandersonrp <wandersonrp.49@hotmail.com>
2024-09-04 23:54:06 -0300

    feat: form to register expense and reload exepenses page

---
+++ src/Web/CashFlow.Web/CashFlow.Web.Client/StringConverters/FormatPaymentType.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Communication.Enums.Expenses;
+
+namespace CashFlow.Web.Client.StringConverters;
+
+public static class FormatPaymentType
+{
+    public static string ToFriendlyString(this PaymentType paymentType)
+    {
+        return paymentType switch
+        {
+            PaymentType.Cash => "Dinheiro",
+            PaymentType.CreditCard => "Cartão de Crédito",
+            PaymentType.DebitCard => "Cartão de Débito",
+            PaymentType.EletronicTransfer => "Transferência Eletrônica",
+            _ => throw new ArgumentOutOfRangeException(nameof(paymentType), paymentType, null)
+        };
+    }
+}
---
+++ src/Web/CashFlow.Web/CashFlow.Web.Shared/Validators/ExpenseFluentValidator.cs
@@ -0,0 +1,37 @@
+﻿using CashFlow.Communication.Requests.Expenses;
+using FluentValidation;
+
+namespace CashFlow.Web.Shared.Validators;
+public class ExpenseFluentValidator : AbstractValidator<RequestExpenseJson>
+{
+    public ExpenseFluentValidator()
+    {
+        RuleFor(x => x.Title)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O título da despesa é obrigatório")
+            .MaximumLength(30)
+            .WithMessage("O título da despesa deve conter no máximo 30 caracteres");        
+                       
+        RuleFor(x => x.Date)
+            .LessThanOrEqualTo(DateTime.UtcNow)
+            .WithMessage("A data da despesa não pode ser futura");
+
+        RuleFor(x => x.Amount)
+            .GreaterThan(0)
+            .WithMessage("O valor da despesa deve ser maior que zero");
+
+        RuleFor(x => x.PaymentType)
+            .IsInEnum()
+            .WithMessage("Tipo de pagamento inválido");
+    }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<RequestExpenseJson>.CreateWithOptions((RequestExpenseJson)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
+}
